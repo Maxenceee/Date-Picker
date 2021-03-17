45cca5d5baa32344e3862c6de96289f0be7029c9
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2021-03-17 16:42:56 +0100

    Initial Commit

---
+++ Date Picker/Date_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_PickerApp.swift
+//  Date Picker
+//
+//  Created by Maxence Gama on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Date_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
